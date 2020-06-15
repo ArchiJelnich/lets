90270223d48278199fd7b53f1b7db5916123ecab
Archi <archi@MacBook-Pro-User-2.local>
2020-06-15 16:36:23 +0300

    add y-2

---
+++ y-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+using namespace std;
+
+
+int main()
+{
+    int bin[10] = {0, 0, 1, 1, 0, 0, 1, 1, 1, 1};
+    set <int> lengths;
+    int current_v = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (bin[i]==0)
+        {
+            current_v = 0;
+        }
+
+        else
+            {
+            current_v++;
+        }
+
+        lengths.insert(current_v);
+
+    }
+
+
+    set <int> :: iterator it = lengths.end();
+    it--;
+
+
+    cout << *it << endl;
+
+
+
+
+return 0;
+
+
+
+
+}
+
